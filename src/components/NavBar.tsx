6632bcb5f627dd779b04ca9beead4e9ea7d95d1a
Andre Vallestero <andrevallestero@gmail.com>
2022-11-24 11:19:52 -0500

    Remove rounded corners

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ import { addSource } from "../util/sources_util";
 export default function NavBar() {
   return (
     <>
-      <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+      <nav class="px-2 sm:px-4 py-2.5 dark:bg-gray-800">
         <div class="container flex flex-wrap items-center justify-between mx-auto">
           <A href="/" class="flex items-center">
             <img src={retroTV} class="h-6 mr-3 sm:h-9" alt="Solid.js Logo" />
@@ -39,7 +39,7 @@ export default function NavBar() {
           </button>
 
           <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-800 dark:border-gray-700">
               <li>
                 <A
                   href="/groupwatch"
