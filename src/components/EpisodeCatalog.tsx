f7417b836f95fb394f99d03dd0ff369804ef72fb
Caynze <aarijwahab@gmail.com>
2022-11-24 22:19:24 -0500

    added episodecatalog

---
+++ src/components/EpisodeCatalog.tsx
@@ -0,0 +1,11 @@
+import { Component } from "solid-js";
+
+const EpisodeCatalog: Component = () => {
+  return (
+    <div>
+      <h2>EpisodeCatalog</h2>
+    </div>
+  );
+};
+
+export default EpisodeCatalog;
