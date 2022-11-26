ddf2476ba9867a052a4b42441a14d950dc708a9e
Andre Vallestero <andrevallestero@gmail.com>
2022-11-26 15:43:00 -0500

    Use built-in mergeSearchString

--- src/components/QueryA.tsx
+++ src/components/QueryA.tsx
@@ -1,26 +1,21 @@
 import { Component, splitProps, untrack } from "solid-js";
-import { A, AnchorProps, SetParams, useLocation } from "@solidjs/router";
+import {
+  A,
+  AnchorProps,
+  SetParams,
+  useLocation,
+  _mergeSearchString,
+} from "@solidjs/router";
 
 interface QueryAnchorProps extends Omit<AnchorProps, "href"> {
   setParams: SetParams;
-}
-
-export function mergeSearchString(search: string, params: SetParams) {
-  const merged = new URLSearchParams(search);
-  Object.entries(params).forEach(([key, value]) =>
-    value == null || value === ""
-      ? merged.delete(key)
-      : merged.set(key, String(value))
-  );
-  const s = merged.toString();
-  return s ? `?${s}` : "";
 }
 
 // Use replace={true} to have the query keys replace instead of append
 const QueryA: Component<QueryAnchorProps> = (props) => {
   const location = useLocation();
   const searchString = untrack(() =>
-    mergeSearchString(props.replace ? "" : location.search, props.setParams)
+    _mergeSearchString(props.replace ? "" : location.search, props.setParams)
   );
   const [, rest] = splitProps(props, ["setParams", "replace"]);
   return (
